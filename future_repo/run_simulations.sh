9785565d65dc9d9fd59b838ef4db0cbb44af7c7f
legabgob <163336605+legabgob@users.noreply.github.com>
2024-11-28 16:52:41 +0100

    Update run_simulations.sh
    Prepared to run on the cluster

--- future_repo/run_simulations.sh
+++ future_repo/run_simulations.sh
@@ -1,11 +1,13 @@
-#!/run/current-system/sw/bin/bash
+# CHANGE PATH TO THE BASH SHELL FOR THE CLUSTER
+#!/run/current-system/sw/bin/bash 
+# ADD SLURM OPTIONS
 
 # Define the mating strategies
 mating_strategies=("random" "random-relatedness" "relatedness" "genetic_load" "mixed")
 
 # Run the burn-in phase
 echo "Starting burn-in phase..."
-for i in {1..2}; do
+for i in {1..100}; do
     slim -d TotalLength=1000000 -d runID=$i -d NBurn=5000 ./burnin.slim
 done
 echo "Burn-in phase completed."
@@ -13,7 +15,7 @@ echo "Burn-in phase completed."
 # Run the genetic load mating phase
 for j in "${mating_strategies[@]}"; do
     echo "Starting $j mating phase..."
-    for i in {1..2}; do
+    for i in {1..100}; do
         echo "Running replicate $i for $j mating strategy..."
         slim -d TotalLength=1000000 -d runID=$i -d NBurn=5000 -d NGen=100 -d Mating=\"$j\" "./$j.slim"
         echo "Completed replicate $i for $j mating strategy."
