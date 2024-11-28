5003a4636d328970f1fba0dd4a53af3c992c28c0
Douhan Wicht <douhan.wicht@gmail.com>
2024-11-28 16:49:41 +0100

    FINIIIIII

--- burnin/burnin.sh
+++
@@ -1,9 +0,0 @@
-#!/run/current-system/sw/bin/bash
-
-# Run the burn-in phase
-echo "Running burn-in phase..."
-for i in {1..100}; do
-    slim -d TotalLength=1000000 -d runID=$i -d NBurn=10000 burnin.slim
-done
-
-echo "Burn-in phase completed."
--- future_repo/run_simulations.sh
+++ future_repo/run_simulations.sh
@@ -5,7 +5,7 @@ mating_strategies=("random" "random-relatedness" "relatedness" "genetic_load" "m
 
 # Run the burn-in phase
 echo "Starting burn-in phase..."
-for i in {1..3}; do
+for i in {1..2}; do
     slim -d TotalLength=1000000 -d runID=$i -d NBurn=5000 ./burnin.slim
 done
 echo "Burn-in phase completed."
@@ -13,7 +13,7 @@ echo "Burn-in phase completed."
 # Run the genetic load mating phase
 for j in "${mating_strategies[@]}"; do
     echo "Starting $j mating phase..."
-    for i in {1..3}; do
+    for i in {1..2}; do
         echo "Running replicate $i for $j mating strategy..."
         slim -d TotalLength=1000000 -d runID=$i -d NBurn=5000 -d NGen=100 -d Mating=\"$j\" "./$j.slim"
         echo "Completed replicate $i for $j mating strategy."
