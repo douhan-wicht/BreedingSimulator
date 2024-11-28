f28f09f68d1235155ccbd7c5e6ecebc36b39083d
Douhan Wicht <douhan.wicht@gmail.com>
2024-11-28 16:57:16 +0100

    Update run_simulations.sh

--- run_simulations.sh
+++ run_simulations.sh
@@ -8,7 +8,7 @@ mating_strategies=("random" "random-relatedness" "relatedness" "genetic_load" "m
 # Run the burn-in phase
 echo "Starting burn-in phase..."
 for i in {1..100}; do
-    slim -d TotalLength=1000000 -d runID=$i -d NBurn=10000 ./burnin.slim
+    slim -d TotalLength=1000000 -d runID=$i -d NBurn=10000 ./3_SLiM-Scripts/burnin.slim
 done
 echo "Burn-in phase completed."
 
