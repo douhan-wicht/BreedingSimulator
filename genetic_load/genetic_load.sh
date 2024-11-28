ffd70b2cf5d080d22ab46ca7448134ed2ee56a2a
Douhan Wicht <douhan.wicht@gmail.com>
2024-11-28 08:26:43 +0100

    example run for random mating

--- genetic_load/genetic_load.sh
+++ genetic_load/genetic_load.sh
@@ -6,7 +6,7 @@ mating_strategies=("genetic_load")
 # Run the burn-in phase
 echo "Starting burn-in phase..."
 for i in {1..3}; do
-    slim -d TotalLength=1000000 -d runID=$i -d NBurn=10000 ../burnin/burnin.slim
+    slim -d TotalLength=1000000 -d runID=$i -d NBurn=5000 ../burnin/burnin.slim
 done
 echo "Burn-in phase completed."
 
@@ -14,7 +14,7 @@ echo "Burn-in phase completed."
 echo "Starting genetic load mating phase..."
 for i in {1..3}; do
     for j in "${mating_strategies[@]}"; do
-        slim -d TotalLength=1000000 -d runID=$i -d NBurn=10000 -d NGen=100 -d Mating='"genetic_load"' ./genetic-load_low-budget_regime.slim
+        slim -d TotalLength=1000000 -d runID=$i -d NBurn=5000 -d NGen=100 -d Mating='"genetic_load"' ./genetic-load_low-budget_regime.slim
     done
 done
 echo "Genetic load mating phase completed."
