130b43a8a42400f7081c8a182fc1ced0354fe072
legabgob <163336605+legabgob@users.noreply.github.com>
2024-11-28 16:24:30 +0100

    Bash script to run all simulations
    change the number of replicates by changing the 3 in
    
    for i in {1..3}; do

--- future_repo/run_simulations.sh
+++ future_repo/run_simulations.sh
@@ -1,7 +1,7 @@
 #!/run/current-system/sw/bin/bash
 
 # Define the mating strategies
-mating_strategies=("genetic_load")
+mating_strategies=("random" "random-relatedness" "relatedness" "genetic_load" "mixed")
 
 # Run the burn-in phase
 echo "Starting burn-in phase..."
@@ -11,10 +11,12 @@ done
 echo "Burn-in phase completed."
 
 # Run the genetic load mating phase
-echo "Starting genetic load mating phase..."
-for i in {1..3}; do
-    for j in "${mating_strategies[@]}"; do
-        slim -d TotalLength=1000000 -d runID=$i -d NBurn=5000 -d NGen=100 -d Mating='"genetic_load"' ./genetic_load.slim
+for j in "${mating_strategies[@]}"; do
+    echo "Starting $j mating phase..."
+    for i in {1..3}; do
+        echo "Running replicate $i for $j mating strategy..."
+        slim -d TotalLength=1000000 -d runID=$i -d NBurn=5000 -d NGen=100 -d Mating=\"$j\" "./$j.slim"
+        echo "Completed replicate $i for $j mating strategy."
     done
+    echo "$j mating phase completed."
 done
-echo "Genetic load mating phase completed."
